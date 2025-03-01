3f88cf452af5e85fea0bb1cd9deae94dc247fe4d
unknown <noe.condado1@gmail.com>
2025-03-01 17:04:37 -0500

    complete signin/signout

--- userauths/views.py
+++ userauths/views.py
@@ -1,8 +1,22 @@
+# Responsible for redirecting users to other pages and rendering the pages
 from django.shortcuts import redirect, render
 from userauths.forms import UserRegistrationForm
-# Allow user toi be logged in automatically into site if signed up
-from django.contrib.auth import login, authenticate
+
+# Allow user to be logged in automatically into site if signed up
+from django.contrib.auth import authenticate, login
+
+#Responsible for logging the user out of their account
+from django.contrib.auth import logout
+
+# Responsible for prompting urgent messages
 from django.contrib import messages 
+
+from django.conf import settings
+
+# What is the point of this variable? Well if you go to bookstore/settings.py 
+# you'll scroll down and find AUTH_USER_MODEL which is connected to "userauths.User"
+# We will use this for our try catch 
+User = settings.AUTH_USER_MODEL
 
 # Create your views here.
 
@@ -36,3 +50,43 @@ def registerView(request):
         'form': form,
     }
     return render(request, "userauths/sign-up.html", context)
+
+def loginView(request):
+    # If a user is already logged in, we want to redirect them to the homepage
+    if request.user.is_authenticated:
+        messages.warning(request, f"Hey, you're already logged in!")
+        return redirect("core:index")
+    
+    if request.method == "POST":
+
+        # Below is basically the same as; email = form = LoginForm(request.POST)
+        # It grabs the email the user inputs
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        ######### PLEASE PUT IN A TRY-CATCH #########
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.warning(request, f"User with {email} doesn't exist!")
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are logged in.")
+            return redirect("core:index")
+        else: 
+            messages.warning(request, "User Does Not Exist, create an account.")
+
+    context = {
+
+    }
+
+    return render(request, "userauths/sign-in.html", context)
+
+
+def logoutView(request):
+      logout(request)
+      messages.success(request, "You logged out.")
+      return redirect("userauths:sign-in")
