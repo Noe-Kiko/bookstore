330e0eb5f5a95a27a4a33e4dbd2625eb1564fccc
Noe Condado-Lino <noecondado@Noes-MacBook-Pro.local>
2025-04-23 23:56:36 -0400

    fixing settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -83,9 +83,6 @@ INSTALLED_APPS = [
     
     # App to integrate paypal
     'paypal.standard.ipn',
-
-    'ckeditor',
-    'ckeditor_uploader',
 ]
 
 MIDDLEWARE = [
@@ -129,8 +126,7 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': os.path.join('/home', 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
